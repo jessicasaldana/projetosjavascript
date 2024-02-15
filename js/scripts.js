ba972dc2f685c5b08fe67c9155c037608504cf2b
Jessica Saldaña <jessicarsaldana@hotmail.com>
2024-02-15 01:31:45 -0300

    add função do botão na seção projetos do index

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,12 @@ const navMenu = document.querySelector('.nav-menu');
 menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
+
+// Seleciona o botão pelo seu ID
+const botao = document.getElementById('botao');
+
+// Adiciona um ouvinte de evento para o evento 'click'
+botao.addEventListener('click', function() {
+    // Redireciona para outra página
+    window.location.href = '#';
+});
